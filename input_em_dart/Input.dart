3af386ebbde740174d9b046b237e924be046ec41
Judson Paiva <judson.paiva@hotmail.com>
2022-01-15 23:48:59 +0100

    importando Biblioteca de entrada e saída de dados

---
+++ input_em_dart/Input.dart
@@ -0,0 +1,24 @@
+
+/*
+ * Fazer um algoritmo que receba a idade da pessoa, se a idade 
+ * for maior ou igual a 18 ele é maior de idade; Se não for ele não
+ * é maior de idade
+*/
+import 'dart:io'
+
+main(){
+
+  var idade = 24;
+
+
+
+  if(idade >= 18){
+    print("Maior de idade");
+  }
+  else
+  {
+    print("Menor de idade");
+  }
+
+
+}
