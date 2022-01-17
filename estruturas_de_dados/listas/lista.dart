e9decf0a89771dc3c1f2ddb1ae5cf49667a3f248
Judson Paiva <judson.paiva@hotmail.com>
2022-01-17 01:19:05 +0100

    projecto lista de compras

--- estruturas_de_dados/listas/lista.dart
+++ estruturas_de_dados/listas/lista.dart
@@ -2,8 +2,16 @@
 main(){
 
   var lista = ["Jéssica", "Leonor", "Alice"];
-
+  lista.add("Judson");
+  lista.add("Quissanga");
+  print(lista);
+  print(lista[1]);
   print(lista.length);
+
+  lista.remove("Quissanga");
+  print(lista);
+
+  List <String> listaNomes = [];
 
 }
 
